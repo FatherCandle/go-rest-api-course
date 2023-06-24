9efd9cab15defd90af8607c2ab313bbb9478cf67
Fathercandle <avnervolpert@gmail.com>
2023-06-24 22:04:46 +0300

    Added update and delete comment dal

--- internal/db/comment.go
+++ internal/db/comment.go
@@ -7,21 +7,21 @@ import (
 
 	"github.com/FatherCandle/go-rest-api-course/internal/comment"
 
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 )
 
 type CommentRow struct {
-	ID string
-	Slug sql.NullString
-	Body sql.NullString
+	ID     string
+	Slug   sql.NullString
+	Body   sql.NullString
 	Author sql.NullString
 }
 
 func convertCommentRowToComment(c CommentRow) comment.Comment {
 	return comment.Comment{
-		ID: c.ID,
-		Slug: c.Slug.String,
-		Body: c.Body.String,
+		ID:     c.ID,
+		Slug:   c.Slug.String,
+		Body:   c.Body.String,
 		Author: c.Author.String,
 	}
 }
@@ -30,10 +30,10 @@ func (d *Database) GetComment(ctx context.Context, uuid string) (comment.Comment
 	var cmtRow CommentRow
 	row := d.Client.QueryRowContext(
 		ctx,
-	`SELECT id, slug, body, author
+		`SELECT id, slug, body, author
 	FROM comments
 	WHERE id = $1`,
-	uuid,
+		uuid,
 	)
 	err := row.Scan(&cmtRow.ID, &cmtRow.Slug, &cmtRow.Body, &cmtRow.Author)
 	if err != nil {
@@ -66,4 +66,41 @@ func (d *Database) PostComment(ctx context.Context, cmt comment.Comment) (commen
 	}
 
 	return cmt, nil
+}
+
+func (d *Database) DeleteComment(ctx context.Context, id string) error {
+	_, err := d.Client.ExecContext(
+		ctx,
+		`DELETE FROM comments where id = $1`,
+		id)
+	if err != nil {
+		return fmt.Errorf("failed to delete comment from database: %w", err)
+	}
+	return nil
+}
+
+func (d *Database) UpdateComment(ctx context.Context, id string, cmt comment.Comment) (comment.Comment, error) {
+	cmtRow := CommentRow{
+		ID:     id,
+		Slug:   sql.NullString{String: cmt.Slug, Valid: true},
+		Body:   sql.NullString{String: cmt.Body, Valid: true},
+		Author: sql.NullString{String: cmt.Author, Valid: true},
+	}
+	rows, err := d.Client.NamedQueryContext(
+		ctx,
+		`UPDATE comments SET
+		slug = :slug,
+		body = :body,
+		author = :author
+		WHERE id = :id`,
+		cmtRow,
+	)
+	if err != nil {
+		return comment.Comment{}, fmt.Errorf("failed to update comment: %s due to %w", id, err)
+	}
+	if err := rows.Close(); err != nil {
+		return comment.Comment{}, fmt.Errorf("failed to close rows: %w", err)
+	}
+
+	return convertCommentRowToComment(cmtRow), nil
 }
