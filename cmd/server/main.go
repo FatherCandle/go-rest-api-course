51a6555242335edd32767f93c040f3a937d6e816
Fathercandle <avnervolpert@gmail.com>
2023-06-24 19:01:43 +0300

    Added db connection logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/FatherCandle/go-rest-api-course/internal/db"
+)
 
 // Run is going ot be responsible for the instantitation and startup of our go application
 func Run() error {
 	fmt.Println("Starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect ot the database")
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
